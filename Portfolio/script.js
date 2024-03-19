318fc676d6d6045ba56e57e8a4f5dc74e8aa4251
Shreyansh2912 <shreyanshshah2912@gmail.com>
2024-03-19 18:50:02 +0530

    data

---
+++ Portfolio/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    console.log("j");
+    $("button").t(function () {
+    console.log("a");
+        $("body").css({"background-color":"black","color":"aliceblue"})
+    })
+    $("h1").
+})
